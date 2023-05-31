3914e5ead2dd8673d392ac0d58846f399c40d8d1
21MH1A04K2 <98155484+21MH1A04K2@users.noreply.github.com>
2023-05-31 10:44:10 +0530

    def prime(n):     if n<2:         return False     for i in range(2,n):         if n%i==0:             return False     return True x=int(input()) c=0 y=map(int,input().split()) z=int(input()) for i in y:     if prime(i):         if i<=z:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,16 @@
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+x=int(input())
+c=0
+y=map(int,input().split())
+z=int(input())
+for i in y:
+    if prime(i):
+        if i<=z:
+            c+=1
+print(c)
