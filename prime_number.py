0308d8694318b4b7676b5069f78c108431bd63b2
21MH1A04K2 <98155484+21MH1A04K2@users.noreply.github.com>
2023-06-03 12:15:00 +0530

    def prime(n):     if n<2:         return False     for i in range(2,n):         if n%i==0:             return False     return True a=int(input()) if prime(a):     print("prime") else:     print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,12 @@
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+a=int(input())
+if prime(a):
+    print("prime")
+else:
+    print("not a prime")
