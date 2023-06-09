5a30d0f2125a2fd2a34c25eb2fcc89b27ece6db3
21MH1A04K2 <98155484+21MH1A04K2@users.noreply.github.com>
2023-06-09 13:42:14 +0530

    n=int(input()) arr=list(map(int,input().split())) d={} c=[] for i in arr:     if i not in d:         d[i]=1     else:         d[i]+=1 for i,j in d.items():     if j==1:         c.append(i) if len(c)==0:     print(-1) else:     print(*c)

---
+++ Display_unique_values_in_an_Array.py
@@ -0,0 +1,18 @@
+n=int(input())
+arr=list(map(int,input().split()))
+d={}
+c=[]
+for i in arr:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+for i,j in d.items():
+    if j==1:
+        c.append(i)
+if len(c)==0:
+    print(-1)
+else:
+    print(*c)
+
+   
